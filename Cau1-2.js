501f9b3b58d0229946eade53c5cdefea381b39a9
quyet.h <babacaca1103@gmail.com>
2022-07-16 03:45:48 +0700

    update

---
+++ Cau1-2.js
@@ -0,0 +1,4 @@
+// 1. 	- Data generator: https://generatedata.com/ xong dùng json to sql
+// 	- Tối Ưu Query bằng đánh chỉ mục index
+
+// 2. Độ phức tạp của thuật toán Sort N*M: N*M*log(M) + M*N*log(N)
